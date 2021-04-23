9c05f3da7976756b10b10574dc82620196a70640
Vick-Bay <50940714+Vick-Bay@users.noreply.github.com>
2021-04-23 17:18:29 +0400

    layout changes

--- src/Features/Focus/Focus.js
+++ src/Features/Focus/Focus.js
@@ -8,31 +8,32 @@ export const Focus = () => {
   return (
     <View style={styles.container}>
       <View style={styles.titleContainer}>
-        <View style={styles.inputContainer}>
-          <Text style={styles.title}>What would you like to focus on?</Text>
-        </View>
-        <TextInput />
+        <Text style={styles.title}>What would you like to focus on?</Text>
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput style={{ flex: 1, marginRight: 20 }} />
         <RoundedButton title="+" size={50} />
       </View>
-    </View>
+    </View >
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1
   },
   titleContainer: {
     flex: 0.5,
     padding: 16,
     justifyContent: 'center'
   },
-  inputContainer: {
-    paddingBottom: 25
-  },
   title: {
     color: 'white',
     fontWeight: 'bold',
     fontSize: 24
+  },
+  inputContainer: {
+    paddingTop: 20,
+    flexDirection: 'row'
   },
 });
