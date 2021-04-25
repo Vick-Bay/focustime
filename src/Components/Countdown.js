7d726ba746f67c5d6051dc03b0ef31f575d1c01f
Vick Bay <levicking@hotmail.com>
2021-04-25 13:33:43 +0400

    add timer functionality

--- src/Components/Countdown.js
+++ src/Components/Countdown.js
@@ -23,10 +23,13 @@ export const Countdown = ({
   }
 
   useEffect(() => {
+    if (isPaused) {
+      return;
+    }
     interval.current = setInterval(countDown, 1000)
 
     return () => clearInterval(interval.current)
-  }, [])
+  }, [isPaused])
 
   const [millis, setMillis] = useState(minutesToMillis(minutes))
 
--- src/Features/timer/Timer.js
+++ src/Features/timer/Timer.js
@@ -1,22 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, StyleSheet, Text } from 'react-native';
 
 
 import { colors } from '../../utils/colors';
 import { spacingSizes } from '../../utils/sizes';
 import { Countdown } from '../../Components/Countdown';
+import { RoundedButton } from '../../Components/RoundedButton'
 
 
 export const Timer = ({ focusSubject }) => {
-
+  const [isStarted, setIsStarted] = useState(false);
   return (
     <View style={styles.container}>
       <View style={styles.countdown}>
-        <Countdown />
+        <Countdown isPaused={!isStarted} />
       </View>
       <View style={{ paddingTop: spacingSizes.xxl }}>
         <Text style={styles.title}>Focusing on: </Text>
         <Text style={styles.task}>{focusSubject}</Text>
+      </View>
+      <View style={styles.buttonWrapper}>
+        {isStarted ? (
+          <RoundedButton title="pause" onPress={() => setIsStarted(false)} />
+        ) : (
+          <RoundedButton title="start" onPress={() => setIsStarted(true)} />
+        )}
       </View>
     </View>
   )
@@ -39,5 +47,11 @@ const styles = StyleSheet.create({
     flex: 0.5,
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  buttonWrapper: {
+    flex: 0.3,
+    padding: 15,
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 })
